994aaf1886ca14ff7a33ef6f9f21435ced5bf4c4
Cam Graff <graffcameron@gmail.com>
2021-11-24 18:28:53 -0600

    Remove caching

--- lua/cmp_nvim_ultisnips/snippets.lua
+++ lua/cmp_nvim_ultisnips/snippets.lua
@@ -1,14 +1,9 @@
 local M = {}
 
-local ft_snippets_cache = {}
 
 function M.load()
-  if ft_snippets_cache[vim.bo.filetype] == nil then
-    vim.F.npcall(vim.call, 'UltiSnips#SnippetsInCurrentScope', 1)
-    ft_snippets_cache[vim.bo.filetype] = vim.g.current_ulti_dict_info
-  end
-
-  return ft_snippets_cache[vim.bo.filetype]
+  vim.F.npcall(vim.call, 'UltiSnips#SnippetsInCurrentScope', 1)
+  return vim.g.current_ulti_dict_info
 end
 
 function M.get_snippet_preview(user_data)
